dd5e139f1fd9255137b459f926c64985cca5e93e
AlBelHub <125362352+AlBelHub@users.noreply.github.com>
2023-10-10 17:39:24 +0300

    Шаблоны данных

---
+++ Portal_ts/src/tempdata/Announcements.ts
@@ -0,0 +1,51 @@
+const ExampleAnouncementData = [
+  {
+    id: "1",
+    header: "header1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloribus fuga! Dolores rem sequi tempore ullam vitae at molestiae maiores atque nemo sunt ea ratione recusandae saepe autem pariatur ex suscipit asperiores sit error, in laudantium distinctio. Fugit dolor dolore necessitatibus nisi harum reprehenderit officia ea, distinctio quos excepturi illum!",
+    timestamp: new Date(2023, 2, 3, 17, 53),
+  },
+  {
+    id: "2",
+    header: "header1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloribus fuga! Dolores rem sequi tempore ullam vitae at molestiae maiores atque nemo sunt ea ratione recusandae saepe autem pariatur ex suscipit asperiores sit error, in laudantium distinctio. Fugit dolor dolore necessitatibus nisi harum reprehenderit officia ea, distinctio quos excepturi illum!",
+    timestamp: new Date(2023, 4, 6, 17, 53),
+  },
+  {
+    id: "3",
+    header: "header1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloribus fuga! Dolores rem sequi tempore ullam vitae at molestiae maiores atque nemo sunt ea ratione recusandae saepe autem pariatur ex suscipit asperiores sit error, in laudantium distinctio. Fugit dolor dolore necessitatibus nisi harum reprehenderit officia ea, distinctio quos excepturi illum!",
+    timestamp: new Date(2023, 3, 20, 18, 53),
+  },
+  {
+    id: "4",
+    header: "header1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloribus fuga! Dolores rem sequi tempore ullam vitae at molestiae maiores atque nemo sunt ea ratione recusandae saepe autem pariatur ex suscipit asperiores sit error, in laudantium distinctio. Fugit dolor dolore necessitatibus nisi harum reprehenderit officia ea, distinctio quos excepturi illum!",
+    timestamp: new Date(2022, 6, 4, 13, 22),
+  },
+  {
+    id: "5",
+    header: "header1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloribus fuga! Dolores rem sequi tempore ullam vitae at molestiae maiores atque nemo sunt ea ratione recusandae saepe autem pariatur ex suscipit asperiores sit error, in laudantium distinctio. Fugit dolor dolore necessitatibus nisi harum reprehenderit officia ea, distinctio quos excepturi illum!",
+    timestamp: new Date(2023, 11, 9, 10, 13),
+  },
+  {
+    id: "6",
+    header: "header1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloribus fuga! Dolores rem sequi tempore ullam vitae at molestiae maiores atque nemo sunt ea ratione recusandae saepe autem pariatur ex suscipit asperiores sit error, in laudantium distinctio. Fugit dolor dolore necessitatibus nisi harum reprehenderit officia ea, distinctio quos excepturi illum!",
+    timestamp: new Date(2023, 12, 30, 9, 46),
+  },
+  {
+    id: "7",
+    header: "header1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. A, doloribus fuga! Dolores rem sequi tempore ullam vitae at molestiae maiores atque nemo sunt ea ratione recusandae saepe autem pariatur ex suscipit asperiores sit error, in laudantium distinctio. Fugit dolor dolore necessitatibus nisi harum reprehenderit officia ea, distinctio quos excepturi illum!",
+    timestamp: new Date(2023, 7, 6, 5, 15),
+  },
+];
