aabe0857e435f0cb621d4154dc266b04ef5c179f
AlBelHub <125362352+AlBelHub@users.noreply.github.com>
2023-10-10 17:42:02 +0300

    Добавил поп-ап меню

--- Portal_ts/src/components/Topbar.tsx
+++ Portal_ts/src/components/Topbar.tsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import ProfileICON from "../assets/profile.svg";
 
 export default function Topbar() {
+  const [settingsOpen, setSettingsOpen] = useState(false);
+
   return (
     <div className="topbar">
       <div className="topbar__items">
         <div className="topbar__brand-container">
-          <div className="topbar__logo">1</div>
-          <p className="toppbar__name">Название компании</p>
+          <Link to="/" className="topbar__logo topbar__link"></Link>
+          <Link to="/" className="toppbar__name topbar__link">
+            НАЗВАНИЕ
+          </Link>
         </div>
-        <div className="search-bar b-radius">
-          1<div className="search-bar__icon">1</div>
-        </div>
+        <input className="search-bar b-radius border" placeholder="Поиск..." />
+        {/* <div type="text" className="search-bar__icon"/> */}
         <div className="user-data">
-          <div className="user-data__name">userName</div>
-          <div className="user-data__photo">1</div>
+          <div className="user-data__name">Алексей Михайлович</div>
+          <div
+            className="user-data__photo"
+            onClick={() => setSettingsOpen(!settingsOpen)}
+          >
+            {/* <img src={ProfileICON} alt="profile" /> */}
+          </div>
+          {settingsOpen && (
+            <div className="user-data__settings b-radius">
+              <Link to="/" className="settings__link b-radius">
+                Профиль
+                <div className="img"></div>
+              </Link>
+              <Link to="/" className="settings__link b-radius">
+                Темная тема
+                <button type="button" className="dark-theme settings__button" />
+              </Link>
+              <Link to="/auth" className="settings__link b-radius">
+                Выход
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
