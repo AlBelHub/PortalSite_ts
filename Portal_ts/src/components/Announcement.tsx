251cd2177d43c54c44d8ae6b6299fea0a94e694f
AlBelHub <125362352+AlBelHub@users.noreply.github.com>
2023-10-18 16:38:21 +0300

    Мелкие правки

--- Portal_ts/src/components/Announcement.tsx
+++ Portal_ts/src/components/Announcement.tsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function Announcement() {
   return (
     <>
-    {/* Разобраться с ограничением количества текста */}
+      {/* Разобраться с ограничением количества текста */}
       <div className="announcements__item announcements_m-p b-radius">
         <h1>Header</h1>
-        <p>Lorem  adipisicing elit. Eligendi, sequi?</p>
+        <p>Lorem adipisicing elit. Eligendi, sequi?</p>
       </div>
     </>
   );
