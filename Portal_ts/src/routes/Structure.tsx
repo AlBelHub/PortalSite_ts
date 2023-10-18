1d0c69d699b6c2998e38e6044b3b68fb29442738
AlBelHub <125362352+AlBelHub@users.noreply.github.com>
2023-10-18 16:38:52 +0300

    Отображение орг.структуры

--- Portal_ts/src/routes/Structure.tsx
+++ Portal_ts/src/routes/Structure.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+import {ExampleStructureObject} from '../tempdata/Structure';
+
 export default function Structure() {
   return (
     <>
       <h1>Structure</h1>
+
+      {
+        ExampleStructureObject.data.map(unit => {
+          return(
+            <div className="unit" key={unit.UnitName}>
+              <p className="unit__name">{unit.UnitName}</p>
+              {unit.Empl.map(empl => <div key={empl} className="unit__empl">{empl}</div>  )}
+            </div>
+          );
+        })
+      }
+
     </>
   );
 }
