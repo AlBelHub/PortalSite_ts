1869dcb1f4eb8fdd2ddc21bfc17f6d6f54305dc2
AlBelHub <125362352+AlBelHub@users.noreply.github.com>
2023-10-23 17:55:09 +0300

    Требуется доработка

---
+++ Portal_ts/src/components/BookingForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/form.scss';
+
+export default function BookingForm() {
+    return(
+        <>
+            <div className="form-container">
+                <form>
+                    <div className="p">123</div>
+                </form>
+            </div>
+        </>
+    );
+}
--- Portal_ts/src/routes/Orders.tsx
+++ Portal_ts/src/routes/Orders.tsx
@@ -1,9 +1,77 @@
 import React from "react";
 
+import "../styles/orders.scss";
+
 export default function Orders() {
   return (
     <>
-      <h1>Orders</h1>
+      <div className="orders-container">
+        <div className="filters">
+          <div className="filter__item"></div>
+        </div>
+        <div className="order-header">
+          <p className="order__header">Заявки</p>
+          <p className="filter-tag">Земляной Вал</p>
+        </div>
+        <div className="order-items">
+          <div className="order__item">
+            <p className="order__text">Когда</p>
+            <p className="order__text">Кому</p>
+            <p className="order__text">Кто</p>
+            <p className="order__text">Откуда</p>
+            <p className="order__text">Состояние</p>
+          </div>
+          <div className="order__item">
+            <p className="order__text">Когда</p>
+            <p className="order__text">Кому</p>
+            <p className="order__text">Кто</p>
+            <p className="order__text">Откуда</p>
+            <p className="order__text">Состояние</p>
+          </div>
+          <div className="order__item">
+            <p className="order__text">Когда</p>
+            <p className="order__text">Кому</p>
+            <p className="order__text">Кто</p>
+            <p className="order__text">Откуда</p>
+            <p className="order__text">Состояние</p>
+          </div>
+          <div className="order__item">
+            <p className="order__text">Когда</p>
+            <p className="order__text">Кому</p>
+            <p className="order__text">Кто</p>
+            <p className="order__text">Откуда</p>
+            <p className="order__text">Состояние</p>
+          </div>
+          <div className="order__item">
+            <p className="order__text">Когда</p>
+            <p className="order__text">Кому</p>
+            <p className="order__text">Кто</p>
+            <p className="order__text">Откуда</p>
+            <p className="order__text">Состояние</p>
+          </div>
+          <div className="order__item">
+            <p className="order__text">Когда</p>
+            <p className="order__text">Кому</p>
+            <p className="order__text">Кто</p>
+            <p className="order__text">Откуда</p>
+            <p className="order__text">Состояние</p>
+          </div>
+          <div className="order__item">
+            <p className="order__text">Когда</p>
+            <p className="order__text">Кому</p>
+            <p className="order__text">Кто</p>
+            <p className="order__text">Откуда</p>
+            <p className="order__text">Состояние</p>
+          </div>
+          <div className="order__item">
+            <p className="order__text">Когда</p>
+            <p className="order__text">Кому</p>
+            <p className="order__text">Кто</p>
+            <p className="order__text">Откуда</p>
+            <p className="order__text">Состояние</p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
