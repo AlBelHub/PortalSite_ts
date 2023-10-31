b5b89d3b65545e14e178e7e7724b9daeacbd8410
AlBelHub <125362352+AlBelHub@users.noreply.github.com>
2023-10-31 17:26:21 +0300

    Незаконченное

--- Portal_ts/src/routes/Errands.tsx
+++ Portal_ts/src/routes/Errands.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Errands() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    async function fetchApi() {
+      const response = await fetch("http://localhost:3000");
+      const json = await response.json();
+      console.log(json);
+    }
+    fetchApi();
+  }, []);
+
   return (
     <>
       <h1>Errands</h1>
